463dbe16027dfa3f4dbc4bb173568422e45eaaf8
theopmw <theowrightdev@gmail.com>
2022-01-14 09:15:39 +0000

    Add ExpenseFilter component, filter year logic and CSS

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value="2029">2029</option>
+          <option value="2028">2028</option>
+          <option value="2027">2027</option>
+          <option value="2026">2026</option>
+          <option value="2025">2025</option>
+          <option value="2024">2024</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
